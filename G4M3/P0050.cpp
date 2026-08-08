12f114df1af9a7810654687d363ef91b26aeafc6
TheoNogueiraVirginio <theonogueiravirginio@gmail.com>
2026-08-07 23:48:14 -0300

    feat(g4m3): add problem P0050 (implementation)

---
+++ G4M3/P0050.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    int x; cin >> x;
+
+    for (int i=1; i<=x; i++) {
+        cout << i << '\n';
+    }
+}
+
+int main() {
+    fast;
+    solve();
+
+    return 0;
+}
