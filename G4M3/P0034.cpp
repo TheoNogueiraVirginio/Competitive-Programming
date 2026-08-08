4e4c2b4f882ba59d1b06d0c5ecab1ca65f4d50f6
TheoNogueiraVirginio <theonogueiravirginio@gmail.com>
2026-08-07 23:47:29 -0300

    feat(g4m3): add problem P0034 (implementation)

---
+++ G4M3/P0034.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    int dummy; cin >> dummy;
+
+    cout << "VALENDO!\n";
+}
+
+int main() {
+    fast;
+    solve();
+
+    return 0;
+}
