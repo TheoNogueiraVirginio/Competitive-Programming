8c872dbb7a54c440e9efee4bc6bda5ef4d2a3856
TheoNogueiraVirginio <theonogueiravirginio@gmail.com>
2026-09-06 23:55:56 -0300

    feat(g4m3): add problem P0042 (implementation)
    Solve 'Dando oi ao mundo!' using standard output in O(1) time

---
+++ G4M3/P0042.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+void solve() {
+    cout << "Hello World!\n";
+}
+
+int main() {
+    fast;
+    solve();
+    
+    return 0;
+}
