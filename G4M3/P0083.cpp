6de0c44cb3f42e832d3051de2405da0eed19a310
TheoNogueiraVirginio <theonogueiravirginio@gmail.com>
2026-09-10 23:52:35 -0300

    feat(g4m3): add problem P0083 (math)
    Solve 'Função de Ativação: ReLU' using conditional evaluation in O(1) time

---
+++ G4M3/P0083.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+void solve() {
+    int x; cin >> x;
+
+    cout << max(x,0) << '\n';
+}
+
+int main() {
+    fast;
+    solve();
+
+    return 0;
+}
